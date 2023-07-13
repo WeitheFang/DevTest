d85041746a174a283a340ab5fcd9c4d2d5851b6c
Wei-Chi Fang <wei@wei-chis-mbp.home>
2023-07-12 22:16:25 -0400

    question 2 answer

--- question1/main.ts
+++ question1/main.ts
@@ -1,24 +1,22 @@
 export function isValidWalk(walk: string[]) {
-    if (walk.length !== 10){
-        return false
+  if (walk.length !== 10) {
+    return false;
+  }
+
+  let x = 0;
+  let y = 0;
+
+  for (const dir of walk) {
+    if (dir === "n") {
+      y += 1;
+    } else if (dir === "s") {
+      y -= 1;
+    } else if (dir === "e") {
+      x += 1;
+    } else {
+      x -= 1;
     }
+  }
 
-    let x = 0;
-    let y = 0;
-
-    for (const dir of walk){
-        if(dir ==='n'){
-            y+=1;
-        }else if(dir ==='s'){
-            y-=1;
-        }else if(dir ==='e'){
-            x+=1;
-        }else{
-            x-=1;
-        }
-    }
-
-    return x===0 && y===0
-  
+  return x === 0 && y === 0;
 }
-
--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,18 @@
 export function findOutlier(integers: number[]): number {
-    
+  const evenNumbers: number[] = [];
+  const oddNumbers: number[] = [];
+
+  for (const num of integers) {
+    if (num % 2 === 0) {
+      evenNumbers.push(num);
+    } else {
+      oddNumbers.push(num);
+    }
   }
+
+  if (evenNumbers.length === 1) {
+    return evenNumbers[0];
+  } else {
+    return oddNumbers[0];
+  }
+}
