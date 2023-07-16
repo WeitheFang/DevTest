549b3fd3ee86b4f20e231b2fa37fec160d7f0222
Wei-Chi Fang <wei@wei-chis-mbp.home>
2023-07-16 15:39:28 -0400

    fix bugs and pass test

--- question3/main.ts
+++ question3/main.ts
@@ -6,8 +6,16 @@ export class Connect4 {
   constructor() {
     // Good luck
     //creates a 2D array with 6 rows and 7 columns, where each element is initialized with the value 0.
-    this.grid = Array.from({ length: 6 }, () => Array(7).fill(0));
-    this.currentPlayer = 1;
+    this.grid = [];
+
+    // Initialize the grid with 6 rows and 7 columns filled with zeros.
+    for (let row = 0; row < 6; row++) {
+      this.grid[row] = [];
+      for (let col = 0; col < 7; col++) {
+        this.grid[row][col] = 0;
+      }
+    }
+    this.currentPlayer = 2;
     this.isGameOver = false;
   }
 
@@ -22,19 +30,20 @@ export class Connect4 {
     }
 
     if (this.isColumnFull(col)) {
-      return "Column full! Choose another column.";
+      return "Column full!";
     }
 
     this.dropDisc(col);
 
     if (this.checkForWin()) {
       this.isGameOver = true;
-      return `Player ${this.currentPlayer} wins!`;
+      return `Player ${this.currentPlayer === 1 ? 2 : 1} wins!`;
     }
 
+    const currentPlayer = this.currentPlayer;
     this.switchPlayers();
 
-    return `Player ${this.currentPlayer} has a turn.`;
+    return `Player ${this.currentPlayer} has a turn`;
   }
 
   isColumnFull(col: number): boolean {
@@ -69,8 +78,68 @@ export class Connect4 {
 
   //Methods for checking win conditions
 
-  checkHorizontalWin(): boolean {}
-  checkVerticalWin(): boolean {}
-  checkDiagonalUpWin(): boolean {}
-  checkDiagonalDownWin(): boolean {}
+  checkHorizontalWin(): boolean {
+    for (let row = 0; row < 6; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (
+          this.grid[row][col] === this.currentPlayer &&
+          this.grid[row][col + 1] === this.currentPlayer &&
+          this.grid[row][col + 2] === this.currentPlayer &&
+          this.grid[row][col + 3] === this.currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  checkVerticalWin(): boolean {
+    for (let col = 0; col < 7; col++) {
+      for (let row = 0; row < 3; row++) {
+        if (
+          this.grid[row][col] === this.currentPlayer &&
+          this.grid[row + 1][col] === this.currentPlayer &&
+          this.grid[row + 2][col] === this.currentPlayer &&
+          this.grid[row + 3][col] === this.currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  checkDiagonalUpWin(): boolean {
+    // Check for a diagonal line (upward) of four discs.
+    for (let row = 3; row < 6; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (
+          this.grid[row][col] === this.currentPlayer &&
+          this.grid[row - 1][col + 1] === this.currentPlayer &&
+          this.grid[row - 2][col + 2] === this.currentPlayer &&
+          this.grid[row - 3][col + 3] === this.currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  checkDiagonalDownWin(): boolean {
+    // Check for a diagonal line (downward) of four discs.
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 4; col++) {
+        if (
+          this.grid[row][col] === this.currentPlayer &&
+          this.grid[row + 1][col + 1] === this.currentPlayer &&
+          this.grid[row + 2][col + 2] === this.currentPlayer &&
+          this.grid[row + 3][col + 3] === this.currentPlayer
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
